a2e904652d07b1b704885b832f028a77b18a236c
Minhang Xie <minxie97@gmail.com>
2021-11-04 18:34:37 -0700

    7 day forecast

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ const Forecast = require('./classes/forecastclass');
 
 async function handleWeather(req, res) {
 
-    const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${req.query.lat}&lon=${req.query.lon}&key=${process.env.WEATHER_API_KEY}`
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${req.query.lat}&lon=${req.query.lon}&key=${process.env.WEATHER_API_KEY}&days=7`
     try {
         let weatherResults = await axios.get(url);
         let clientWeather = weatherResults.data.data.map(weather => new Forecast(weather.datetime, weather.weather.description));
