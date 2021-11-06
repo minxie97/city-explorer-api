5fe3aa035040a213e70a040d420b950117db0e32
Minhang Xie <minxie97@gmail.com>
2021-11-05 18:16:27 -0700

    set up caches

--- weather.js
+++ weather.js
@@ -1,20 +1,29 @@
 'use strict';
 const axios = require('axios');
 const Forecast = require('./classes/forecastclass');
+const cache = require('./cache.js');
 
 async function handleWeather(req, res) {
 
-    const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${req.query.lat}&lon=${req.query.lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=7`
-    try {
-        let weatherResults = await axios.get(url);
-        let clientWeather = weatherResults.data.data.map(weather => new Forecast(weather));
-        if (clientWeather) {
-            res.status(200).send(clientWeather);
-        } else {
-            res.status(404).send('not found fam');
+    if (cache[req.query.lat, req.query.lon] && (Date.now() - cache[req.query.lat, req.query.lon].timestamp) < 300000) {
+        console.log('weather cache hit!');
+        res.status(200).send(cache[req.query.lat, req.query.lon]);
+    } else {
+        console.log('weather cache miss');
+        const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${req.query.lat}&lon=${req.query.lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=7`
+        try {
+            let weatherResults = await axios.get(url);
+            let clientWeather = weatherResults.data.data.map(weather => new Forecast(weather));
+            if (clientWeather) {
+                cache[req.query.lat, req.query.lon] = clientWeather;
+                cache[req.query.lat, req.query.lon].timestamp = Date.now();
+                res.status(200).send(clientWeather);
+            } else {
+                res.status(404).send('not found fam');
+            }
+        } catch (e) {
+            res.status(500).send('Error: Could not locate weather data on server.')
         }
-    } catch (e) {
-        res.status(500).send('Error: Could not locate weather data on server.')
     }
 }
 
--- yelp.js
+++ yelp.js
@@ -1,31 +1,29 @@
 'use strict';
 const axios = require('axios');
+const Yelp = require('./classes/yelpclass.js');
+const cache = require('./cache.js');
 
 async function handleYelp(req, res) {
 
-    const url = `https://api.yelp.com/v3/businesses/search?location=${req.query.searchQuery}&term=restaurants&limit=${5}`
-
-    try {
-        let yelpResults = await axios.get(url,{ headers: { 'Authorization': `Bearer ${process.env.YELP_API_KEY}` } })
-        let clientYelp = yelpResults.data.businesses.map(food => new Yelp(food));
-        if (clientYelp) {
-            res.status(200).send(clientYelp);
-        } else {
-            res.status(404).send('Error: location not found');
+    if (cache[req.query.city] && (Date.now() - cache[req.query.city].timestamp) < 300000) {
+        console.log('yelp cache hit!');
+        res.status(200).send(cache[req.query.city]);
+    } else {
+        console.log('yelp cache miss');
+        const url = `https://api.yelp.com/v3/businesses/search?location=${req.query.city}&term=restaurants&limit=${5}`
+        try {
+            let yelpResults = await axios.get(url, { headers: { 'Authorization': `Bearer ${process.env.YELP_API_KEY}` } })
+            let clientYelp = yelpResults.data.businesses.map(food => new Yelp(food));
+            if (clientYelp) {
+                cache[req.query.city] = clientYelp;
+                cache[req.query.city].timestamp = Date.now();
+                res.status(200).send(clientYelp);
+            } else {
+                res.status(404).send('Error: location not found');
+            }
+        } catch (e) {
+            res.status(500).send('Nope')
         }
-    } catch (e) {
-        res.status(500).send('Nope')
     }
 }
-
-class Yelp {
-    constructor(obj) {
-        this.name = obj.name;
-        this.imageUrl = obj.image_url;
-        this.price = obj.price;
-        this.rating = obj.rating;
-        this.url = obj.url;
-    }
-}
-
 module.exports = handleYelp;
